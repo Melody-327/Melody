b3fe6cd6248861e1fd0288bd9a0a202f82ebdae6
Melody-327 <2785297892@qq.com>
2025-11-21 21:15:14 +0800

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,9 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -15,7 +18,70 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    bool getUserEditConfiremed() const;
+
+private slots:
+    void on_actionAbout_triggered();
+
+    void on_actionReplace_triggered();
+
+    void on_actionFind_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_textEdit_undoAvailable(bool b);
+
+    void on_textEdit_copyAvailable(bool b);
+
+    void on_textEdit_redoAvailable(bool b);
+
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSaveAs_triggered();
+
+    void on_textEdit_textChanged();
+
+    void on_actionFontColor_triggered();
+
+    void on_actionbackgroundColor_triggered();
+
+    void on_actionFontBackgroundColor_triggered();
+
+    void on_actionLineWrap_triggered();
+
+    void on_actionFont_triggered();
+
+    void on_actionToolBar_triggered();
+
+    void on_actionStatusBar_triggered();
+
+    void on_actionSelectAll_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_textEdit_cursorPositionChanged();
+
 private:
     Ui::MainWindow *ui;
+
+    QLabel statusCursorLabel;
+    QLabel statusLabel;
+
+    QString filePath;
+    bool textChanged;
+
+    bool userEditConfirmed();
 };
 #endif // MAINWINDOW_H
